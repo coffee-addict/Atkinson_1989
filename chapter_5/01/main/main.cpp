39182644a35cb2efac2f4b97eac97fb63ebeb309
Kosuke Sugita <ksk.sgt+github@gmail.com>
2019-03-25 15:54:56 -0400

    add 5-1

---
+++ chapter_5/01/main/main.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i,s,e) for(int (i)=(s);(i)<(int)(e);(i)++)
+#define REP(i,e) FOR(i,0,e)
+#define each(it,c) for(__typeof((c).begin()) it=(c).begin();it!=(c).end();it++)
+#define all(o) (o).begin(), (o).end()
+#define pb(x) push_back(x)
+#define mp make_pair
+#define mt make_tuple
+#define t0(t) get<0>((t))
+#define t1(t) get<1>((t))
+#define t2(t) get<2>((t))
+
+typedef long long ll;
+const int NF = 5;
+const int M = 9;
+const int N = 1<<M;
+double a[NF] = {0, 0, -4, 0, 0};
+double b[NF] = {1, 1, 4, acos(-1.0)*2, acos(-1.0)}; 
+double xs[N+1], ys[N+1];
+double res[2][M+1];
+
+double fa(double x) {
+  return exp(-x*x);
+}
+
+double fb(double x) {
+  return pow(x,2.5);
+}
+
+double fc(double x) {
+  return 1.0 / (1.0 + x*x);
+}
+
+double fd(double x) {
+  return 1.0 / (2.0 + cos(x));
+}
+
+double fe(double x) {
+  return exp(x) * cos(x*4);
+}
+
+double (*fn_p[])(double) = {fa, fb, fc, fd, fe};
+
+void init(int i) {
+  double h = (b[i] - a[i])/N;
+  memset(res, 0, sizeof(res));
+  REP(j,N+1) {
+    xs[j] = a[i] + h*j;
+    ys[j] = fn_p[i](xs[j]);
+  }
+}
+
+int main() {
+  REP(i,NF) {
+    init(i);
+    int m = 1, n = 1<<m;
+    while (m <= M) {
+      double h = (b[i] - a[i])/n;
+      res[0][m] = (ys[0] + ys[n])*0.5;
+      FOR(j,1,n) res[0][m] += ys[j*N/n];
+      res[0][m] *= h;
+      m++;
+      n *= 2;
+    }
+    printf("case (%c):\n", 'a'+i);
+    puts("n e r");
+    FOR(j,2,M) res[1][j] = (res[0][j] - res[0][j-1])/(res[0][j+1] - res[0][j]);
+    FOR(j,1,M+1) printf("%d %f, %f\n", 1<<j, res[0][j], res[1][j]);
+  }
+  return 0;
+}
